cf6122a7633d96ed015972080043cfebe9f73da5
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-24 21:03:15 +0200

    add modal on click img

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,10 @@
 const uri = "https://lanciweb.github.io/demo/api/pictures";
-const cardsList = document.getElementById("cards-list");
+const listOfCards = document.getElementById("cards-list");
+const modalImgLabel = document.getElementById("img-label");
+const modalBody = document.getElementById("modal-body-img");
+
+console.log(modalImgLabel);
+console.log(modalBody.src);
 
 axios.get(uri).then((responce) => {
   const imges = responce.data;
@@ -7,12 +12,19 @@ axios.get(uri).then((responce) => {
   imges.forEach((image) => {
     cardsHtml += generatedCard(image);
   });
-  cardsList.innerHTML = cardsHtml;
+  listOfCards.innerHTML = cardsHtml;
+  const imgCard = document.querySelectorAll(".img-card");
+  imgCard.forEach((img) => {
+    img.addEventListener("click", () => {
+      modalImgLabel.innerText = "testo immagine";
+      modalBody.src = img.src;
+    });
+  });
 });
 
 const generatedCard = (card) => {
   return `<div class="col-12 col-md-6 col-xl-4">
-            <div class="card my-4 p-3">
+            <div class="card my-4 p-3" data-bs-toggle="modal" data-bs-target="#img-on-focus">
               <div class="pin">
                 <img src="./img/pin.svg" alt="" />
               </div>
