b9a038386f2587566e8c48d5f648bfa0f0f0dd63
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-24 15:24:41 +0200

    generate cards from Js

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,28 @@
-console.log("Js ok");
-
 const uri = "https://lanciweb.github.io/demo/api/pictures";
+const cardsList = document.getElementById("cards-list");
 
 axios.get(uri).then((responce) => {
-  console.log(responce.data);
+  const imges = responce.data;
+  let cardsHtml = "";
+  imges.forEach((image) => {
+    cardsHtml += generatedCard(image);
+  });
+  cardsList.innerHTML = cardsHtml;
 });
+
+const generatedCard = (card) => {
+  return `<div class="col-12 col-md-6 col-xl-4">
+            <div class="card my-4 p-3">
+              <div class="pin">
+                <img src="./img/pin.svg" alt="" />
+              </div>
+              <div class="card-header">
+                <img class="img-card" src="${card.url}" alt="" />
+              </div>
+              <div class="card-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+                quaerat!
+              </div>
+            </div>
+          </div>`;
+};
