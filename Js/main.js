e03ef174abae406493b38f09bde93de22f6b5db3
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-28 14:43:41 +0200

    fix add event listener on card

--- Js/main.js
+++ Js/main.js
@@ -3,27 +3,29 @@ const listOfCards = document.getElementById("cards-list");
 const modalImgLabel = document.getElementById("img-label");
 const modalBody = document.getElementById("modal-body-img");
 
-console.log(modalImgLabel);
-console.log(modalBody.src);
-
 axios.get(uri).then((responce) => {
-  const imges = responce.data;
+  const cards = responce.data;
   let cardsHtml = "";
-  imges.forEach((image) => {
-    cardsHtml += generatedCard(image);
+  cards.forEach((card) => {
+    cardsHtml += generatedCard(card);
   });
   listOfCards.innerHTML = cardsHtml;
-  const imgCard = document.querySelectorAll(".img-card");
-  imgCard.forEach((img) => {
-    img.addEventListener("click", () => {
-      modalImgLabel.innerText = "testo immagine";
-      modalBody.src = img.src;
+
+  const cardNodes = document.querySelectorAll(".cardimg");
+  cardNodes.forEach((card) => {
+    card.addEventListener("click", () => {
+      const cardId = parseInt(card.getAttribute("data-card-id"));
+      cards.find((card) => {
+        if (card.id === cardId) {
+          modalImgLabel.innerText = card.title;
+          modalBody.src = card.url;
+        }
+      });
     });
   });
 });
-
 const generatedCard = (card) => {
-  return `<div class="col-12 col-md-6 col-xl-4">
+  return `<div class="cardimg col-12 col-md-6 col-xl-4" data-card-id="${card.id}">
             <div class="card my-4 p-3" data-bs-toggle="modal" data-bs-target="#img-on-focus">
               <div class="pin">
                 <img src="./img/pin.svg" alt="" />
